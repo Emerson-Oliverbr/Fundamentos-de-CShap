4f0d2b63565142de04effe1b6c21d46a9685541d
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-03-31 21:09:52 -0300

    Correção constantes

--- LinguagemCShap/Constantes/Program.cs
+++ LinguagemCShap/Constantes/Program.cs
@@ -6,9 +6,11 @@ namespace Constantes
     {
         static void Main(string[] args)
         {
-            const IDADE_MINIMA = 10;
-            const IDADE_MAXIMA = 50;
+            const int IDADE_MINIMA = 10;
+            const int IDADE_MAXIMA = 50;
 
+            Console.WriteLine(IDADE_MINIMA);
+            Console.WriteLine(IDADE_MAXIMA);
         }       
     }
 }
