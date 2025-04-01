886b23c3e0169d5230ca35171b474608917e247c
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-03-31 21:04:47 -0300

    Aprendendo constantes

---
+++ LinguagemCShap/Constantes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Constantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const IDADE_MINIMA = 10;
+            const IDADE_MAXIMA = 50;
+
+        }       
+    }
+}
+
---
+++ LinguagemCShap/Variaveis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Variaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 40;
+            string nome = "Emerson";
+            var sobreNome = "Oliveira";
+
+            Console.WriteLine($"Nome: {nome}, Sobrenome: {sobreNome}, Idade: {idade}");
+        }       
+    }
+}
+
