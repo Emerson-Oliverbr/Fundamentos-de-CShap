37a1acf03c932810d0449725ba621a289d212426
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-04-01 22:15:14 -0300

    Tipo Nulable

---
+++ LinguagemCShap/NullabelTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NullabelTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? idade = null;
+            byte? numero = null;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(numero);
+        }       
+    }
+}
+
