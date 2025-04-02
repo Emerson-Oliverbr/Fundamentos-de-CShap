5ea5608a3bdbdabef8575a1c768f91af29aaa2bf
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-04-01 22:22:02 -0300

    Alias

---
+++ LinguagemCShap/Alias/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Alias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 25; // Alias
+            int32 idade = 25; //Tipo
+        }       
+    }
+}
+
