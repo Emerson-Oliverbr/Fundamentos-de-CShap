66f470a12547d94729a3a2ad3701641b0e642bb8
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-04-01 21:48:40 -0300

    Aprendendo variaveis

--- LinguagemCShap/Variaveis/Program.cs
+++ LinguagemCShap/Variaveis/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Variaveis
 {
@@ -6,11 +7,28 @@ namespace Variaveis
     {
         static void Main(string[] args)
         {
+            char sexo = 'M';
             int idade = 40;
-            string nome = "Emerson";
+            string primeiroNome = "Emerson";
             var sobreNome = "Oliveira";
+            Console.WriteLine($"Nome Completo: {primeiroNome} {sobreNome}");
+            Console.WriteLine($"Nome: {primeiroNome}, Sobrenome: {sobreNome}, Idade: {idade}, e sexo {sexo}");
+            
+            float altura  = 1.98f;
+            double medida =  12.75;
+            decimal salario = 2800.00M;
 
-            Console.WriteLine($"Nome: {nome}, Sobrenome: {sobreNome}, Idade: {idade}");
+            Console.WriteLine($"Salario = R$ {salario.ToString("F2",CultureInfo.InvariantCulture)}, medida em centimetro: {medida.ToString("F2",CultureInfo.InvariantCulture)}, e altura = {altura.ToString("F2",CultureInfo.InvariantCulture)}");
+
+            bool usuarioJaCadastrado = false;
+            bool pagamentoRecebido = true;
+
+            Console.WriteLine(usuarioJaCadastrado);
+            Console.WriteLine(pagamentoRecebido);
+
+            var nomeDaEmpresa = "Magazine Luiza";
+
+            Console.WriteLine("Nome da empresa: " + nomeDaEmpresa);
         }       
     }
 }
