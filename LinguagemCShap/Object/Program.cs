9e1818ef471b18836c23ae6f04a5fc4bb4ce95ee
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-04-01 22:00:00 -0300

    Aprendendo o tipo Object

--- LinguagemCShap/Object/Program.cs
+++ LinguagemCShap/Object/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object idade = 25; // sera do tipo object
+            object nome = "Emerson"; // sera do tipo object
+        }       
+    }
+}
